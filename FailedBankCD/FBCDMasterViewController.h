87b9887bbc6bd10b8532ff4b78a9a763773287a5
beepscore <support@beepscore.com>
2013-12-10 22:43:15 -0800

    Add FBCDMasterViewController.

---
+++ FailedBankCD/FBCDMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBCDMasterViewController.h
+//  FailedBankCD
+//
+//  Created by Steve Baker on 12/10/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBCDMasterViewController : UITableViewController
+
+@end
